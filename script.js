a4a6c9eadd62d2c8660ca33feaf042ba20ae70c2
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2023-12-15 20:24:28 +0530

    added full version of ROCKPAPERSCISSORS

--- script.js
+++ script.js
@@ -1,93 +1,206 @@
-const playBoard = document.querySelector(".snake-play-board");
-const scoreElement = document.querySelector(".snake-score");
-const highScoreElement = document.querySelector(".snake-high-score");
-const controls = document.querySelectorAll(".controls i");
+// const playBoard = document.querySelector(".snake-play-board");
+// const scoreElement = document.querySelector(".snake-score");
+// const highScoreElement = document.querySelector(".snake-high-score");
+// const controls = document.querySelectorAll(".controls i");
 
-let gameOver = false;
-let foodX, foodY;
-let snakeX = 5, snakeY = 5;
-let velocityX = 0, velocityY = 0;
-let snakeBody = [];
-let setIntervalId;
-let score = 0;
+// let gameOver = false;
+// let foodX, foodY;
+// let snakeX = 5, snakeY = 5;
+// let velocityX = 0, velocityY = 0;
+// let snakeBody = [];
+// let setIntervalId;
+// let score = 0;
 
-// Getting high score from the local storage
-let highScore = localStorage.getItem("high-score") || 0;
-highScoreElement.innerText = `High Score: ${highScore}`;
+// // Getting high score from the local storage
+// let highScore = localStorage.getItem("high-score") || 0;
+// highScoreElement.innerText = `High Score: ${highScore}`;
 
-const updateFoodPosition = () => {
-    // Passing a random 1 - 30 value as food position
-    foodX = Math.floor(Math.random() * 30) + 1;
-    foodY = Math.floor(Math.random() * 30) + 1;
-}
+// const updateFoodPosition = () => {
+//     // Passing a random 1 - 30 value as food position
+//     foodX = Math.floor(Math.random() * 30) + 1;
+//     foodY = Math.floor(Math.random() * 30) + 1;
+// }
 
-const handleGameOver = () => {
-    // Clearing the timer and reloading the page on game over
-    clearInterval(setIntervalId);
-    alert("Game Over! Press OK to replay...");
-    location.reload();
-}
+// const handleGameOver = () => {
+//     // Clearing the timer and reloading the page on game over
+//     clearInterval(setIntervalId);
+//     alert("Game Over! Press OK to replay...");
+//     location.reload();
+// }
 
-const changeDirection = e => {
-    // Changing velocity value based on key press
-    if(e.key === "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
-    } else if(e.key === "ArrowDown" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
-    } else if(e.key === "ArrowLeft" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
-    } else if(e.key === "ArrowRight" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
-    }
-}
+// const changeDirection = e => {
+//     // Changing velocity value based on key press
+//     if(e.key === "ArrowUp" && velocityY != 1) {
+//         velocityX = 0;
+//         velocityY = -1;
+//     } else if(e.key === "ArrowDown" && velocityY != -1) {
+//         velocityX = 0;
+//         velocityY = 1;
+//     } else if(e.key === "ArrowLeft" && velocityX != 1) {
+//         velocityX = -1;
+//         velocityY = 0;
+//     } else if(e.key === "ArrowRight" && velocityX != -1) {
+//         velocityX = 1;
+//         velocityY = 0;
+//     }
+// }
 
-// Calling changeDirection on each key click and passing key dataset value as an object
-controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+// // Calling changeDirection on each key click and passing key dataset value as an object
+// controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
-const initGame = () => {
-    if(gameOver) return handleGameOver();
-    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+// const initGame = () => {
+//     if(gameOver) return handleGameOver();
+//     let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-    // Checking if the snake hit the food
-    if(snakeX === foodX && snakeY === foodY) {
-        updateFoodPosition();
-        snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
-        score++; // increment score by 1
-        highScore = score >= highScore ? score : highScore;
-        localStorage.setItem("high-score", highScore);
-        scoreElement.innerText = `Score: ${score}`;
-        highScoreElement.innerText = `High Score: ${highScore}`;
-    }
-    // Updating the snake's head position based on the current velocity
-    snakeX += velocityX;
-    snakeY += velocityY;
+//     // Checking if the snake hit the food
+//     if(snakeX === foodX && snakeY === foodY) {
+//         updateFoodPosition();
+//         snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+//         score++; // increment score by 1
+//         highScore = score >= highScore ? score : highScore;
+//         localStorage.setItem("high-score", highScore);
+//         scoreElement.innerText = `Score: ${score}`;
+//         highScoreElement.innerText = `High Score: ${highScore}`;
+//     }
+//     // Updating the snake's head position based on the current velocity
+//     snakeX += velocityX;
+//     snakeY += velocityY;
     
-    // Shifting forward the values of the elements in the snake body by one
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
-    }
-    snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+//     // Shifting forward the values of the elements in the snake body by one
+//     for (let i = snakeBody.length - 1; i > 0; i--) {
+//         snakeBody[i] = snakeBody[i - 1];
+//     }
+//     snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
 
-    // Checking if the snake's head is out of wall, if so setting gameOver to true
-    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-        return gameOver = true;
-    }
+//     // Checking if the snake's head is out of wall, if so setting gameOver to true
+//     if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+//         return gameOver = true;
+//     }
 
-    for (let i = 0; i < snakeBody.length; i++) {
-        // Adding a div for each part of the snake's body
-        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-        // Checking if the snake head hit the body, if so set gameOver to true
-        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
-            gameOver = true;
-        }
-    }
-    playBoard.innerHTML = html;
+//     for (let i = 0; i < snakeBody.length; i++) {
+//         // Adding a div for each part of the snake's body
+//         html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+//         // Checking if the snake head hit the body, if so set gameOver to true
+//         if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+//             gameOver = true;
+//         }
+//     }
+//     playBoard.innerHTML = html;
+// }
+
+// updateFoodPosition();
+// setIntervalId = setInterval(initGame, 100);
+// document.addEventListener("keyup", changeDirection);
+
+
+
+// ROCK PAPER SCISSOR
+
+
+
+var threeHands = {paper: "./Assets/rockpaperscissors/Paper.png",scissors: "./Assets/rockpaperscissors/Scissors.png",rock: "./Assets/rockpaperscissors/Rock.png"}
+var RPS_score= 0
+
+console.log(threeHands);
+
+function pickUserHand(userInp){
+console.log(userInp);
+
+//hiding hands page
+
+var hideHands =document.querySelector('.RPS-hands')
+hideHands.style.display = 'none'
+
+//showing contest page
+
+var showResult = document.querySelector('.RPS-contest')
+showResult.style.display = 'flex'
+
+//setting img src according to user picks
+
+var userhand =document.getElementById('userPickImage')
+if(userInp == 'paper'){
+    console.log(threeHands.paper);
+    userhand.src = `${threeHands.paper}`    
+
+}else if(userInp == 'scissors'){
+
+    userhand.src = `${threeHands.scissors}`    
+    
+}else if(userInp == 'rock'){
+
+    userhand.src = `${threeHands.rock}`    
+    
 }
 
-updateFoodPosition();
-setIntervalId = setInterval(initGame, 100);
-document.addEventListener("keyup", changeDirection);
+//picked computer choice randomly
+
+let threeOpts = ['paper','scissors','rock']
+let cpRandom= threeOpts[Math.floor(Math.random() * threeOpts.length)]
+
+
+
+//setting img src according to computer picks
+
+var computerhand = document.getElementById('computerPickImage')
+
+ if(cpRandom == 'paper'){
+
+    computerhand.src = `${threeHands.paper}`    
+
+}else if(cpRandom == 'scissors'){
+
+    computerhand.src = `${threeHands.scissors}`    
+    
+}else if(cpRandom == 'rock'){
+
+    computerhand.src = `${threeHands.rock}`    
+    
+}
+
+//setting user Wins or tie in h1 tag
+
+var decision =document.querySelector('.RPS-decision h1')
+
+if(userInp == cpRandom){
+    decision.innerText = "ITS A TIE!"
+}else if(userInp=='paper' && cpRandom=='rock') {
+    decision.innerText = "YOU WIN!"
+    score++
+}else if(userInp=='rock' && cpRandom=='paper') {
+    decision.innerText = "COMPUTER WIN!"
+}else if(userInp=='scissors' && cpRandom=='paper') {
+    decision.innerText = "YOU WIN!"
+    score++
+}else if(userInp=='paper' && cpRandom=='scissors') {
+    decision.innerText = "COMPUTER WIN!"
+}else if(userInp=='scissors' && cpRandom=='rock') {
+    decision.innerText = "COMPUTER WIN!"
+}else if(userInp=='rock' && cpRandom=='scissors') {
+    decision.innerText = "YOU WIN!"
+    score++
+}
+
+//updating score
+
+var updatingScore=document.querySelector('.RPS-score h1')
+
+updatingScore.innerText=`${score}`
+
+
+}
+
+function restartGame(){
+    
+//showing hands page
+
+var hideHands =document.querySelector('.RPS-hands')
+hideHands.style.display = 'flex'
+
+//hiding contest page
+
+var showResult = document.querySelector('.RPS-contest')
+showResult.style.display = 'none'
+
+console.log(score);
+}
