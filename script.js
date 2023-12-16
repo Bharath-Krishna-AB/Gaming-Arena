180bbd77e29f59f831421a7cf4460b33196621c4
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2023-12-16 11:26:02 +0530

    launching for github

--- script.js
+++ script.js
@@ -1,96 +1,98 @@
-// const playBoard = document.querySelector(".snake-play-board");
-// const scoreElement = document.querySelector(".snake-score");
-// const highScoreElement = document.querySelector(".snake-high-score");
-// const controls = document.querySelectorAll(".controls i");
+const playBoard = document.querySelector(".snake-play-board");
+const scoreElement = document.querySelector(".snake-score");
+const highScoreElement = document.querySelector(".snake-high-score");
+const controls = document.querySelectorAll(".controls i");
 
-// let gameOver = false;
-// let foodX, foodY;
-// let snakeX = 5, snakeY = 5;
-// let velocityX = 0, velocityY = 0;
-// let snakeBody = [];
-// let setIntervalId;
-// let score = 0;
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
 
-// // Getting high score from the local storage
-// let highScore = localStorage.getItem("high-score") || 0;
-// highScoreElement.innerText = `High Score: ${highScore}`;
+// Getting high score from the local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
 
-// const updateFoodPosition = () => {
-//     // Passing a random 1 - 30 value as food position
-//     foodX = Math.floor(Math.random() * 30) + 1;
-//     foodY = Math.floor(Math.random() * 30) + 1;
-// }
+const updateFoodPosition = () => {
+    // Passing a random 1 - 30 value as food position
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
 
-// const handleGameOver = () => {
-//     // Clearing the timer and reloading the page on game over
-//     clearInterval(setIntervalId);
-//     alert("Game Over! Press OK to replay...");
-//     location.reload();
-// }
+const handleGameOver = () => {
+    // Clearing the timer and reloading the page on game over
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay...");
+    location.reload();
+}
 
-// const changeDirection = e => {
-//     // Changing velocity value based on key press
-//     if(e.key === "ArrowUp" && velocityY != 1) {
-//         velocityX = 0;
-//         velocityY = -1;
-//     } else if(e.key === "ArrowDown" && velocityY != -1) {
-//         velocityX = 0;
-//         velocityY = 1;
-//     } else if(e.key === "ArrowLeft" && velocityX != 1) {
-//         velocityX = -1;
-//         velocityY = 0;
-//     } else if(e.key === "ArrowRight" && velocityX != -1) {
-//         velocityX = 1;
-//         velocityY = 0;
-//     }
-// }
+const changeDirection = e => {
+    console.log(e);
+    // Changing velocity value based on key press
+    if(e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if(e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if(e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
 
-// // Calling changeDirection on each key click and passing key dataset value as an object
-// controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+// Calling changeDirection on each key click and passing key dataset value as an object
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
-// const initGame = () => {
-//     if(gameOver) return handleGameOver();
-//     let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-//     // Checking if the snake hit the food
-//     if(snakeX === foodX && snakeY === foodY) {
-//         updateFoodPosition();
-//         snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
-//         score++; // increment score by 1
-//         highScore = score >= highScore ? score : highScore;
-//         localStorage.setItem("high-score", highScore);
-//         scoreElement.innerText = `Score: ${score}`;
-//         highScoreElement.innerText = `High Score: ${highScore}`;
-//     }
-//     // Updating the snake's head position based on the current velocity
-//     snakeX += velocityX;
-//     snakeY += velocityY;
+const initGame = () => {
+    if(gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    // Checking if the snake hit the food
+    if(snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+        score++; // increment score by 1
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
+    }
+    // Updating the snake's head position based on the current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
     
-//     // Shifting forward the values of the elements in the snake body by one
-//     for (let i = snakeBody.length - 1; i > 0; i--) {
-//         snakeBody[i] = snakeBody[i - 1];
-//     }
-//     snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+    // Shifting forward the values of the elements in the snake body by one
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
 
-//     // Checking if the snake's head is out of wall, if so setting gameOver to true
-//     if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-//         return gameOver = true;
-//     }
+    // Checking if the snake's head is out of wall, if so setting gameOver to true
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
 
-//     for (let i = 0; i < snakeBody.length; i++) {
-//         // Adding a div for each part of the snake's body
-//         html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-//         // Checking if the snake head hit the body, if so set gameOver to true
-//         if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
-//             gameOver = true;
-//         }
-//     }
-//     playBoard.innerHTML = html;
-// }
+    for (let i = 0; i < snakeBody.length; i++) {
+        // Adding a div for each part of the snake's body
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        // Checking if the snake head hit the body, if so set gameOver to true
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = html;
+}
 
-// updateFoodPosition();
-// setIntervalId = setInterval(initGame, 100);
-// document.addEventListener("keyup", changeDirection);
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
 
 
 
@@ -350,104 +352,104 @@
 
 
 
-let btnRef = document.querySelectorAll(".TTT-button-option");
-let popupRef = document.querySelector(".TTT-popup");
-let newgameBtn = document.getElementById("TTT-new-game");
-let restartBtn = document.getElementById("TTT-restart");
-let msgRef = document.getElementById("TTT-message");
-//Winning Pattern Array
-let winningPattern = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [2, 5, 8],
-  [6, 7, 8],
-  [3, 4, 5],
-  [1, 4, 7],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-//Player 'X' plays first
-let xTurn = true;
-let count = 0;
-//Disable All Buttons
-const disableButtons = () => {
-  btnRef.forEach((element) => (element.disabled = true));
-  //enable popup
-  popupRef.classList.remove("TTT-hide");
-};
-//Enable all buttons (For New Game and Restart)
-const enableButtons = () => {
-  btnRef.forEach((element) => {
-    element.innerText = "";
-    element.disabled = false;
-  });
-  //disable popup
-  popupRef.classList.add("TTT-hide");
-};
-//This function is executed when a player wins
-const winFunction = (letter) => {
-  disableButtons();
-  if (letter == "X") {
-    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
-  } else {
-    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
-  }
-};
-//Function for draw
-const drawFunction = () => {
-  disableButtons();
-  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
-};
-//New Game
-newgameBtn.addEventListener("click", () => {
-  count = 0;
-  enableButtons();
-});
-restartBtn.addEventListener("click", () => {
-  count = 0;
-  enableButtons();
-});
-//Win Logic
-const winChecker = () => {
-  //Loop through all win patterns
-  for (let i of winningPattern) {
-    let [element1, element2, element3] = [
-      btnRef[i[0]].innerText,
-      btnRef[i[1]].innerText,
-      btnRef[i[2]].innerText,
-    ];
-    //Check if elements are filled
-    //If 3 empty elements are same and would give win as would
-    if (element1 != "" && (element2 != "") & (element3 != "")) {
-      if (element1 == element2 && element2 == element3) {
-        //If all 3 buttons have same values then pass the value to winFunction
-        winFunction(element1);
-      }
-    }
-  }
-};
-//Display X/O on click
-btnRef.forEach((element) => {
-  element.addEventListener("click", () => {
-    if (xTurn) {
-      xTurn = false;
-      //Display X
-      element.innerText = "X";
-      element.disabled = true;
-    } else {
-      xTurn = true;
-      //Display Y
-      element.innerText = "O";
-      element.disabled = true;
-    }
-    //Increment count on each click
-    count += 1;
-    if (count == 9) {
-      drawFunction();
-    }
-    //Check for win on every click
-    winChecker();
-  });
-});
-//Enable Buttons and disable popup on page load
-window.onload = enableButtons;
+// let btnRef = document.querySelectorAll(".TTT-button-option");
+// let popupRef = document.querySelector(".TTT-popup");
+// let newgameBtn = document.getElementById("TTT-new-game");
+// let restartBtn = document.getElementById("TTT-restart");
+// let msgRef = document.getElementById("TTT-message");
+// //Winning Pattern Array
+// let winningPattern = [
+//   [0, 1, 2],
+//   [0, 3, 6],
+//   [2, 5, 8],
+//   [6, 7, 8],
+//   [3, 4, 5],
+//   [1, 4, 7],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
+// //Player 'X' plays first
+// let xTurn = true;
+// let count = 0;
+// //Disable All Buttons
+// const disableButtons = () => {
+//   btnRef.forEach((element) => (element.disabled = true));
+//   //enable popup
+//   popupRef.classList.remove("TTT-hide");
+// };
+// //Enable all buttons (For New Game and Restart)
+// const enableButtons = () => {
+//   btnRef.forEach((element) => {
+//     element.innerText = "";
+//     element.disabled = false;
+//   });
+//   //disable popup
+//   popupRef.classList.add("TTT-hide");
+// };
+// //This function is executed when a player wins
+// const winFunction = (letter) => {
+//   disableButtons();
+//   if (letter == "X") {
+//     msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+//   } else {
+//     msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+//   }
+// };
+// //Function for draw
+// const drawFunction = () => {
+//   disableButtons();
+//   msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+// };
+// //New Game
+// newgameBtn.addEventListener("click", () => {
+//   count = 0;
+//   enableButtons();
+// });
+// restartBtn.addEventListener("click", () => {
+//   count = 0;
+//   enableButtons();
+// });
+// //Win Logic
+// const winChecker = () => {
+//   //Loop through all win patterns
+//   for (let i of winningPattern) {
+//     let [element1, element2, element3] = [
+//       btnRef[i[0]].innerText,
+//       btnRef[i[1]].innerText,
+//       btnRef[i[2]].innerText,
+//     ];
+//     //Check if elements are filled
+//     //If 3 empty elements are same and would give win as would
+//     if (element1 != "" && (element2 != "") & (element3 != "")) {
+//       if (element1 == element2 && element2 == element3) {
+//         //If all 3 buttons have same values then pass the value to winFunction
+//         winFunction(element1);
+//       }
+//     }
+//   }
+// };
+// //Display X/O on click
+// btnRef.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     if (xTurn) {
+//       xTurn = false;
+//       //Display X
+//       element.innerText = "X";
+//       element.disabled = true;
+//     } else {
+//       xTurn = true;
+//       //Display Y
+//       element.innerText = "O";
+//       element.disabled = true;
+//     }
+//     //Increment count on each click
+//     count += 1;
+//     if (count == 9) {
+//       drawFunction();
+//     }
+//     //Check for win on every click
+//     winChecker();
+//   });
+// });
+// //Enable Buttons and disable popup on page load
+// window.onload = enableButtons;
