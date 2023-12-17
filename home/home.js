c9ec2a3d602d19b1df63e7c8a24d93561794b7c4
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2023-12-18 03:13:01 +0530

    added back btn and made home page perfect with animation

--- home/home.js
+++ home/home.js
@@ -1,6 +1,6 @@
-var typed = new Typed('.auto-input',{
-    strings: ['AMING ARENA'],
-    typeSpeed:100,
-    backSpeed:100,
-    loop: true
-})
+// var typed = new Typed('.auto-input',{
+//     strings: ['AMING ARENA'],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     loop: true
+// })
